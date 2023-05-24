f4cc250bed22fe9e850c0acfe9cf912663ec9d8a
Davin Leong <davinleongg@gmail.com>
2023-05-23 17:57:19 -0700

    Step 9. Corner case 2. If the user clicks on a card that is already matched, do nothing. Step 10. Corner case 3. If the user clicks on a card while two cards are already flipped, do nothing.

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 
 
-
 const setup = () => {
-  let firstCard = undefined
-  let secondCard = undefined
+  let firstCard = null;
+  let secondCard = null;
+  let hasCompare = false;
+
+  function resetCards() {
+    firstCard = null;
+    secondCard = null;
+    console.log("Reset cards");
+  }
+
   $(".card").on(("click"), function () {
 
-    if ($(this).hasClass("flip")) {
+    if ($(this).hasClass("flip") || hasCompare) {
       console.log("same card pressed");
       return;
     }
@@ -14,9 +21,9 @@ const setup = () => {
     $(this).toggleClass("flip");
 
     if (!firstCard)
-      firstCard = $(this).find(".front_face")[0]
+      firstCard = $(this).find(".front_face")[0];
     else {
-      secondCard = $(this).find(".front_face")[0]
+      secondCard = $(this).find(".front_face")[0];
       console.log(firstCard, secondCard);
       if (
         firstCard.src
@@ -24,17 +31,24 @@ const setup = () => {
         secondCard.src
       ) {
         console.log("match")
-        $(`#${firstCard.id}`).parent().off("click")
-        $(`#${secondCard.id}`).parent().off("click")
+        $(`#${firstCard.id}`).parent().off("click");
+        $(`#${secondCard.id}`).parent().off("click");
+        resetCards();
       } else {
         console.log("no match")
+        hasCompare = true;
         setTimeout(() => {
-          $(`#${firstCard.id}`).parent().toggleClass("flip")
-          $(`#${secondCard.id}`).parent().toggleClass("flip")
+          $(`#${firstCard.id}`).parent().toggleClass("flip");
+          $(`#${secondCard.id}`).parent().toggleClass("flip");
+          resetCards();
+          hasCompare = false;
         }, 1000)
+        
       }
     }
   });
 }
+
+
 
 $(document).ready(setup)
